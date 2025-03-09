8a9c3f054e885c99f195ccf99144df34a8bb7a23
pab-h <dev.pab.2020@gmail.com>
2025-03-09 13:02:01 -0300

    faet: uso do velocimetro

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,13 +1,39 @@
 #include <Arduino.h>
 
 #include "setup.hpp"
+#include "Speedometer.hpp"
+#include "HallSensor.hpp"
+#include "config.hpp"
+
+Speedometer speedometer;
+HallSensor hallSensor;
+
+void ISRHallSensorHitWrapper() {
+    speedometer.ISRHallSensorHit();
+}
 
 void setup() {
+    Serial.begin(9600);
 
     setupPins();
 
+    attachInterrupt(
+        digitalPinToInterrupt(HALL_SENSOR_PIN),
+        ISRHallSensorHitWrapper,
+        RISING 
+    );
+
+    speedometer.setRadius(33);
+
 }
 
+
 void loop() {
+
+    Serial.print("Velocity = ");
+    Serial.println(speedometer.getVelocity(), 2);
+    
+    speedometer.updateVelocity();
+    speedometer.resetTrigger();
 
 }
