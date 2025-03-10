fd4321d3c3105a8945c4564e8c205c8fd6cb4f35
pab-h <dev.pab.2020@gmail.com>
2025-03-09 21:23:28 -0300

    feat: odometro

---
+++ firmware/include/Hodometer.hpp
@@ -0,0 +1,33 @@
+#ifndef HODOMETER_HEADER
+
+#define HODOMETER_HEADER
+
+class Hodometer {
+
+    private:
+
+        float radius;
+        float circumferenceSize;
+        float distance;
+        
+        volatile unsigned long lastMeasureTime;
+        unsigned long resetTimeThreshold;
+
+        volatile bool isNewDataAvailable;
+
+        void updateCircumferenceSize();
+
+    public:
+
+        Hodometer();
+
+        void setRadius(float radius);        
+        void resetTrigger();
+        void ISRHallSensorHit();
+        
+        float getDistance();
+        void updateDistance();
+
+    };
+
+#endif
