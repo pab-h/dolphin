482f9861a4a81db1bdb3057d81e9d0a5d4b5a064
pab-h <dev.pab.2020@gmail.com>
2025-03-17 13:54:31 -0300

    feat: código base para a escrita do raio na EEPROM

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
+#include <EEPROM.h>
 
 #include "setup.hpp"
 #include "config.hpp"
 #include "pins.hpp"
 
 #include "sensors/HallSensor.hpp"
+#include "sensors/PushButtonSensor.hpp"
 
 #include "meters/Speedometer.hpp"
 #include "meters/Hodometer.hpp"
@@ -20,6 +22,16 @@ Hodometer hodometer;
 SpeedDisplay speedDisplay(&speedometer);
 DistanceDisplay distanceDisplay(&hodometer);
 
+PushButtonSensor leftButton(LEFT_BUTTON_PIN);
+PushButtonSensor rightButton(RIGHT_BUTTON_PIN);
+
+bool lastConfigState = false;
+bool configState     = false;
+
+unsigned int radiusEEAddress = 0;
+uint8_t radius               = 0; 
+
+
 void ISRHallSensorHitWrapper() {
     
     hodometer.ISRHallSensorHit();
@@ -27,33 +39,108 @@ void ISRHallSensorHitWrapper() {
 
 }
 
+void toggleConfigMode() {
+
+  lastConfigState = configState;
+  configState = !configState;
+
+  Serial.print("Modo de configuração = ");
+  Serial.print(configState ? "ativado" : "desativado");
+  Serial.print("\n");
+
+  if (configState) {
+    Serial.print("Tamanho do raio atual: R = ");
+    Serial.print(radius);
+    Serial.print("cm");
+    Serial.print("\n");
+  }
+
+  if (lastConfigState && !configState) {
+
+    Serial.print("Atualizando o tamanho do raio: R = ");
+    Serial.print(radius);
+    Serial.print("cm");
+    Serial.print("\n");
+
+    EEPROM.write(radiusEEAddress, radius);
+
+    hodometer.setRadius(radius);
+    speedometer.setRadius(radius);
+
+  }
+
+}
+
+void onLeftClick() {
+
+  if (!configState) {
+    return;
+  }
+  
+  radius++;
+
+  Serial.print("Tamanho do raio atual: R = ");
+  Serial.print(radius);
+  Serial.print("cm");
+  Serial.print("\n");
+
+}
+
+void onRightClick() {
+
+  if (!configState) {
+    return;
+  }
+
+  if (radius == 1) {
+    return;
+  }
+
+  radius--;
+
+  Serial.print("Tamanho do raio atual: R = ");
+  Serial.print(radius);
+  Serial.print("cm");
+  Serial.print("\n");
+
+}
+
 void setup() {
 
-    setupPins();
+  Serial.begin(9600);
 
-    distanceDisplay.setup();
-    speedDisplay.setup();
+  radius       = EEPROM.read(radiusEEAddress);
 
-    attachInterrupt(
-        digitalPinToInterrupt(HALL_SENSOR_PIN),
-        ISRHallSensorHitWrapper,
-        RISING 
-    );
+  Serial.print("Tamanho do raio atual [EEPROM]: R = ");
+  Serial.print(radius);
+  Serial.print("cm");
+  Serial.print("\n");
 
-    hodometer.setRadius(33);
-    speedometer.setRadius(33);
+  setupPins();
+
+  distanceDisplay.setup();
+  speedDisplay.setup();
+
+  attachInterrupt(
+      digitalPinToInterrupt(HALL_SENSOR_PIN),
+      ISRHallSensorHitWrapper,
+      RISING 
+  );
+
+  hodometer.setRadius(radius);
+  speedometer.setRadius(radius);
+
+  leftButton.addLongPressListener(toggleConfigMode);
+
+  leftButton.addClickListener(onLeftClick);
+  rightButton.addClickListener(onRightClick);
 
 }
 
 
 void loop() {
 
-    delay(500);
-
-    speedDisplay.update();
-    distanceDisplay.update();
-
-    speedometer.resetTrigger();
-    hodometer.resetTrigger();
+  leftButton.read();
+  rightButton.read();
 
 }
